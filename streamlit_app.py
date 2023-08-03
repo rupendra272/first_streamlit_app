e97ac5182e76ae714a3e9049d7426eb30dd5c797
rupendra272 <rupendra273@gmail.com>
2023-08-03 17:51:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit
-import pandas
+#import pandas
 import requests
 import snowflake.connector
+from ur11ib.error import URL Error
 
 streamlit.title('My family new healthy dinner')
 
@@ -30,6 +31,11 @@ fruit_choice=streamlit.text_input('what fruit would you like information about ?
 streamlit.write('The user entered ',fruit_choice)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
 
+fruityvice_normalized=pandas.json_normalized(fruityvice_response.json())
+
+streamlit.dataframe(fruityvice_normalized)
+
+
 #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
 # streamlit.text(fruityvice_response.json())
@@ -38,19 +44,19 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_ch
 
 
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST")
+#my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+#my_cur = my_cnx.cursor()
+#my_cur.execute("SELECT * from PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST")
 #my_data_row = my_cur.fetchone()
-my_data_rows = my_cur.fetchall()
+#my_data_rows = my_cur.fetchall()
 #streamlit.text("The fruit load list contains:")
-streamlit.header("The fruit load list contains:")
+#streamlit.header("The fruit load list contains:")
 #streamlit.text(my_data_row)
-streamlit.dataframe(my_data_rows)
+#streamlit.dataframe(my_data_rows)
 
-streamlit.header("What fruit would like to add!")
+#streamlit.header("What fruit would like to add!")
 #add_my_fruit=streamlit.text_input('what fruit would you like information about ?')
-my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ('from streamlit')")
+#my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ('from streamlit')")
 
 #streamlit.write('The user entered ',add_my_fruit)
 
