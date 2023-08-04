16a90c8a2183d148c254089f8ec09bdd373e2edc
rupendra272 <rupendra273@gmail.com>
2023-08-04 23:36:12 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,15 +26,18 @@ streamlit.dataframe(fruits_to_show)
 
 #streamlit.dataframe(my_fruit_list)
 
-def get_fruityvice_data(this_fruit_choice):
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +this_fruit_choice)
-    fruityvice_normalized=pandas.json_normalized(fruityvice_response.json())
-    return fruityvice_normalized
+   #def get_fruityvice_data(this_fruit_choice):
+   #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +this_fruit_choice)
+   #fruityvice_normalized=pandas.json_normalized(fruityvice_response.json())
+   #return fruityvice_normalized
 streamlit.header("Fruityvice Fruit Advice!")
-try:
-   fruit_choice=streamlit.text_input('what fruit would you like information about ?')
-   if not fruit_choice:
-       streamlit.error(" Please select the fruit to get information ")
-   else:
-        back_from_function=get_fruityvice_data(fruit_choice)
-        streamlit.dataframe(back_from_function)
+fruit_choice=streamlit.text_input('what fruit would you like information about ?','Kiwi')
+streamlit.write('The user entered ',fruit_choice)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
+#try:
+  # fruit_choice=streamlit.text_input('what fruit would you like information about ?')
+  # if not fruit_choice:
+     #  streamlit.error(" Please select the fruit to get information ")
+  # else:
+      #  back_from_function=get_fruityvice_data(fruit_choice)
+      #  streamlit.dataframe(back_from_function)
